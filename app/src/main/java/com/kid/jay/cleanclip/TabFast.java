18032c4f34bb5c7e0a38c995fc90740aeca0d468
Jay Kid <cyph3rfox@gmail.com>
2018-01-05 00:00:59 +0100

    feat: first version of link preview, yaaay : D
    + Added two libraries: one for image manipulation and one for url content scrapping
    + The app now needs internet permissions since it fetches the contents of the link in the clipboard to generate a preview

--- app/src/main/java/com/kid/jay/cleanclip/TabFast.java
+++ app/src/main/java/com/kid/jay/cleanclip/TabFast.java
@@ -8,10 +8,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.leocardz.link.preview.library.LinkPreviewCallback;
+import com.leocardz.link.preview.library.SourceContent;
+import com.leocardz.link.preview.library.TextCrawler;
+import com.squareup.picasso.Picasso;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
 
 public class TabFast extends Activity {
 
@@ -33,6 +40,9 @@ public class TabFast extends Activity {
             cleanUrl = stripQueryParameters(clipboardURL);
 
             if (!cleanUrl.isEmpty()) {
+
+                startUnfurling(cleanUrl);
+
                 TextView clipboardLinkLabel = findViewById(R.id.clipboardLinkLabel);
                 clipboardLinkLabel.setText(cleanUrl);
             }
@@ -83,5 +93,39 @@ public class TabFast extends Activity {
             needsPersisting = false;
         }
         super.onPause();
+    }
+
+    private void startUnfurling(String url) {
+
+        TextCrawler textCrawler = new TextCrawler();
+
+        LinkPreviewCallback linkPreviewCallback = new LinkPreviewCallback() {
+            @Override
+            public void onPre() {
+                // Might have to do sth here... (?)
+            }
+
+            @Override
+            public void onPos(SourceContent sourceContent, boolean b) {
+                ImageView linkImagePreview = findViewById(R.id.linkImagePreview);
+                TextView linkTitlePreview = findViewById(R.id.linkTitlePreview);
+                TextView linkDescriptionPreview = findViewById(R.id.linkDescriptionPreview);
+                if (sourceContent.getTitle() != null) {
+                    linkTitlePreview.setText(sourceContent.getTitle());
+                }
+                if (sourceContent.getDescription() != null) {
+                    linkDescriptionPreview.setText(sourceContent.getDescription());
+                }
+                List<String> images = sourceContent.getImages();
+                if (images != null && !images.isEmpty()) {
+                    String mainImageUrl = images.get(0);
+                    if (mainImageUrl != null) {
+                        Picasso.with(getApplicationContext()).load(mainImageUrl).into(linkImagePreview);
+                    }
+                }
+            }
+        };
+
+        textCrawler.makePreview( linkPreviewCallback, url);
     }
 }
