37e4beecc0ff207ec248195c5ae541aeb6222e57
Jay Kid <cyph3rfox@gmail.com>
2018-02-25 10:02:04 +0100

    fix: add heading to ShareActivity to give some context

--- app/src/main/java/com/kid/jay/cleanclip/ShareActivity.java
+++ app/src/main/java/com/kid/jay/cleanclip/ShareActivity.java
@@ -2,7 +2,6 @@ package com.kid.jay.cleanclip;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
